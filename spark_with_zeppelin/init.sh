df17c502d1b5610daafb175c1440599668631814
iandow <kayaking@gmail.com>
2019-02-12 15:12:19 -0800

    install zeppelin

--- spark_with_zeppelin/init.sh
+++ spark_with_zeppelin/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install MapR POSIX client in order to use MapR via NFS
-yum install mapr-posix-client-* -y
+yum install unzip mapr-posix-client-* -y
 systemctl restart mapr-posix-client-container
 
 # Disable annoying console email notifications
@@ -9,9 +9,9 @@ postfix stop
 rm -f /var/spool/mail/root
 
 # Download Zeppelin
-wget -P /opt http://apache.claz.org/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz
-tar -C /opt/ -xzf /opt/zeppelin-0.8.1-bin-netinst.tgz
-mv /opt/zeppelin-0.8.1-bin-netinst /opt/zeppelin
+wget -P /opt http://us.mirrors.quenda.co/apache/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz 
+tar -C /opt/ -xzf /opt/zeppelin-0.8.1-bin-all.tgz
+mv /opt/zeppelin-0.8.1-bin-all /opt/zeppelin
 # Configure Zeppelin for YARN, Spark, and webui port 7000
 echo "export ZEPPELIN_PORT=7000" >> /opt/zeppelin/conf/zeppelin-env.sh
 echo "export MASTER=yarn-client" >> /opt/zeppelin/conf/zeppelin-env.sh
@@ -26,9 +26,16 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:7000/api
     sleep 1
 done
 
+#add a license
+wget -P /root -O license.txt https://gist.githubusercontent.com/iandow/03198d1d42c117c564eca5e00033bd36/raw/cd8bac1f3ea56a1862f920aa4d48511a073fcb4e/license.txt
+maprcli license add -license /root/license.txt -is_file true
+
 # Download sample Zeppelin notebook
 wget -P /root/ https://raw.githubusercontent.com/mapr-demos/predictive-maintenance/master/notebooks/zeppelin/RNN%20predictions%20on%20MapR-DB%20data%20via%20Drill.json
 wget -P /root/ https://raw.githubusercontent.com/mapr-demos/mapr-sparkml-streaming-wildfires/master/notebook/Forest%20Fire%20Prediction.json
 # Import notebook into Zeppelin
 curl -X POST http://localhost:7000/api/notebook/import -d @"/root/RNN predictions on MapR-DB data via Drill.json"
 curl -X POST http://localhost:7000/api/notebook/import -d @"/root/Forest Fire Prediction.json"
+
+
+
