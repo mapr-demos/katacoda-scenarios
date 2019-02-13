c0e85a6a2f8aef26790b71a461979c4cc591b323
iandow <kayaking@gmail.com>
2019-02-13 10:41:05 -0800

    refresh hive metastore

--- spark_with_zeppelin/init.sh
+++ spark_with_zeppelin/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Remove Hive metastore in order to avoid some wierd errors
+/opt/mapr/hive/hive-2.3/bin/hive --service metastore --stop
+rm -rf ../bin/metastore_db/
+/opt/mapr/hive/hive-2.3/bin/hive --service metastore --start
+
 # Install MapR POSIX client in order to use MapR via NFS
 yum install unzip mapr-posix-client-* -y
 systemctl restart mapr-posix-client-container
@@ -25,12 +30,6 @@ echo "mapr" | maprlogin password -user mapr
 until $(curl --output /dev/null --silent --head --fail http://localhost:7000/api); do
     sleep 1
 done
-
-# Remove Hive metastore in order to avoid some wierd errors
-/opt/mapr/hive/hive-2.3/bin/hive --service metastore --stop
-rm -rf ../bin/metastore_db/
-/opt/mapr/hive/hive-2.3/bin/hive --service metastore --start
-
 
 ####################################################
 # Install stuff needed for the Forest Fire notebook
