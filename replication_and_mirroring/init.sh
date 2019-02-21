7c2117e4c17572ecebfaf605022983c5427a44dc
iandow <kayaking@gmail.com>
2019-02-21 10:33:02 -0800

    initial commit

--- replication_and_mirroring/init.sh
+++ replication_and_mirroring/init.sh
@@ -20,24 +20,3 @@ systemctl restart mapr-posix-client-container
 postfix stop
 rm -f /var/spool/mail/root
 
-# Download Zeppelin
-wget -P /opt http://us.mirrors.quenda.co/apache/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz 
-tar -C /opt/ -xzf /opt/zeppelin-0.8.1-bin-all.tgz
-mv /opt/zeppelin-0.8.1-bin-all /opt/zeppelin
-# Configure Zeppelin for YARN, Spark, and webui port 7000
-echo "export ZEPPELIN_PORT=7000" >> /opt/zeppelin/conf/zeppelin-env.sh
-echo "export SPARK_HOME=/opt/mapr/spark/spark-2.3.1" >> /opt/zeppelin/conf/zeppelin-env.sh
-echo "export HADOOP_HOME=/opt/mapr/hadoop/hadoop-2.7.0" >> /opt/zeppelin/conf/zeppelin-env.sh
-
-# Start Zeppelin
-/opt/zeppelin/bin/zeppelin-daemon.sh start
-# Wait for Zeppelin start to complete
-until $(curl --output /dev/null --silent --head --fail http://localhost:7000/api); do
-    sleep 1
-done
-
-# Import notebook into Zeppelin
-wget -P /root https://raw.githubusercontent.com/mapr-demos/katacoda-scenarios/master/spark_with_zeppelin/assets/Forest%20Fire%20Prediction.json
-cp assets/Forest*.json /root
-curl -X POST http://localhost:7000/api/notebook/import -d @"/root/Forest Fire Prediction.json"
-
