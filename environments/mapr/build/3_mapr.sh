d2d0b545ce7f7854e8ce628d4567765ece0d8017
Carsten Hufe <carsten.hufe@gmail.com>
2019-04-23 14:14:42 +0200

    Update 3_mapr.sh
    set to latest versions

--- environments/mapr/build/3_mapr.sh
+++ environments/mapr/build/3_mapr.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-export MAPR_ANSIBLE_INVENTORY="ansible_inventory/hosts_mapr_slim"
+export MAPR_ANSIBLE_INVENTORY="ansible_inventory/hosts_mapr_hive_drill"
 export MAPR_ANSIBLE_EXTRA_VARS="disks=/dev/vdb disk_storage_pool_size=1 virtual_machine=True hive_db_type=embedded"
 
 cd /opt/
 git clone https://github.com/mapr-emea/mapr-vagrant-packer.git
 cd mapr-vagrant-packer
-git checkout 866be7512681fd43b405a321d445bcb93756d569
+git checkout 9ec74c52e56a0dbbee325cabb34b5cf94e48e8bc
 
 cd /opt/
 git clone https://github.com/mapr-emea/mapr-ansible.git
 cd mapr-ansible
-git checkout 26c173eb7301b42070133affe0317593066c9548
+git checkout 977b9a0f90684a693fb94debf01020220fc1f079
 ansible-playbook -i ../mapr-vagrant-packer/$MAPR_ANSIBLE_INVENTORY --extra-vars "$MAPR_ANSIBLE_EXTRA_VARS" site-cluster.yml
 
 wget -P /opt http://us.mirrors.quenda.co/apache/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz
+echo "v1" > /tmp/maprimage
