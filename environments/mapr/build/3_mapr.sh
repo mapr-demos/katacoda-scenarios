f30c7df928231140a236c916a9da45390278de7a
Carsten Hufe <chufe@mapr.com>
2019-04-26 12:46:53 +0200

    Fixes for Katacoda image build

--- environments/mapr/build/3_mapr.sh
+++ environments/mapr/build/3_mapr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export MAPR_ANSIBLE_INVENTORY="ansible_inventory/hosts_mapr_hive_drill"
-export MAPR_ANSIBLE_EXTRA_VARS="disks=/dev/vdb disk_storage_pool_size=1 virtual_machine=True hive_db_type=embedded"
+export MAPR_ANSIBLE_EXTRA_VARS="disks=/dev/vdb disk_storage_pool_size=1 virtual_machine=True hive_db_type=embedded security_encryption_rest=False"
 
 cd /opt/
 git clone https://github.com/mapr-emea/mapr-vagrant-packer.git
@@ -11,7 +11,7 @@ git checkout 9ec74c52e56a0dbbee325cabb34b5cf94e48e8bc
 cd /opt/
 git clone https://github.com/mapr-emea/mapr-ansible.git
 cd mapr-ansible
-git checkout 977b9a0f90684a693fb94debf01020220fc1f079
+git checkout katacoda
 ansible-playbook -i ../mapr-vagrant-packer/$MAPR_ANSIBLE_INVENTORY --extra-vars "$MAPR_ANSIBLE_EXTRA_VARS" site-cluster.yml
 
 rm -Rf /opt/mapr-ansible
@@ -21,8 +21,9 @@ sleep 1m
 
 systemctl restart mapr-warden
 
-sleep 5m
+sleep 3m
 
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk/
 export MAPR_TICKETFILE_LOCATION=/opt/mapr/conf/mapruserticket
 
 maprcli volume list -json | jq '.data[].volumename' | xargs -L 1 maprcli volume modify -replication 1 -minreplication 1 -name
@@ -35,4 +36,4 @@ systemctl stop mapr-warden
 systemctl stop mapr-zookeeper
 
 #wget -P /opt http://us.mirrors.quenda.co/apache/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz
-echo "v3" > /tmp/maprimage
+echo "v4" > /tmp/maprimage
