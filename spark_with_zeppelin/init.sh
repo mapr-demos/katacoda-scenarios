573832bd7208922fa25409a628337e32b1968e65
carol mcdonald <caroljmcdonald@gmail.com>
2019-04-26 12:13:30 -0400

    remove hive from init

--- spark_with_zeppelin/init.sh
+++ spark_with_zeppelin/init.sh
@@ -8,9 +8,9 @@ wget -P /root https://raw.githubusercontent.com/mapr-demos/katacoda-scenarios/ma
 maprcli license add -license /root/mapr_license.txt -is_file true
 
 # Remove Hive metastore in order to avoid some wierd errors
-/opt/mapr/hive/hive-2.3/bin/hive --service metastore --stop
-rm -rf /opt/mapr/hive/hive-2.3/bin/metastore_db/
-/opt/mapr/hive/hive-2.3/bin/hive --service metastore --start
+#/opt/mapr/hive/hive-2.3/bin/hive --service metastore --stop
+#rm -rf /opt/mapr/hive/hive-2.3/bin/metastore_db/
+#/opt/mapr/hive/hive-2.3/bin/hive --service metastore --start
 
 # Install MapR POSIX client in order to use MapR via NFS
 yum install unzip mapr-posix-client-* -y
