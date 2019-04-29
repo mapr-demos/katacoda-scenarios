c9468870275e8a7cb8026cb8f0b69480f5bb4f6a
Carsten Hufe <chufe@mapr.com>
2019-04-29 10:37:03 +0200

    moved license and service file to seperate folder, seems that katacoda executes evrything from the folder

--- environments/mapr/build/3_mapr.sh
+++ environments/mapr/build/3_mapr.sh
@@ -43,7 +43,7 @@ maprcli volume list -json | jq '.data[].volumename' | xargs -L 1 maprcli volume 
 maprcli alarm clearall
 
 # Add a mapr license needed for snapshots
-wget -P /tmp https://raw.githubusercontent.com/mapr-demos/katacoda-scenarios/master/environments/mapr/build/mapr_license.txt
+wget -P /tmp https://raw.githubusercontent.com/mapr-demos/katacoda-scenarios/master/envfiles/mapr_license.txt
 maprcli license add -license /tmp/mapr_license.txt -is_file true
 
 echo "Git rev $GIT_REV" > /tmp/maprimage
@@ -61,7 +61,7 @@ echo "export SPARK_HOME=/opt/mapr/spark/spark-2.3.1" >> /opt/zeppelin/conf/zeppe
 echo "export HADOOP_HOME=/opt/mapr/hadoop/hadoop-2.7.0" >> /opt/zeppelin/conf/zeppelin-env.sh
 echo "export SPARK_SUBMIT_OPTIONS=\"--packages graphframes:graphframes:0.7.0-spark2.3-s_2.11\"" >> /opt/zeppelin/conf/zeppelin-env.sh
 
-wget -P /etc/systemd/system/ https://raw.githubusercontent.com/mapr-demos/katacoda-scenarios/master/environments/mapr/build/zeppelin.service
+wget -P /etc/systemd/system/ https://raw.githubusercontent.com/mapr-demos/katacoda-scenarios/master/envfiles/zeppelin.service
 #sudo systemctl enable zeppelin
 
 # Disable annoying console email notifications
