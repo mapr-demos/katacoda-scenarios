a518ae5fad67e0d1de2ba06fcab8f16047bc3400
Carsten Hufe <chufe@mapr.com>
2019-05-03 17:48:06 +0200

    Drill workaround.

--- environments/mapr/build/3_mapr.sh
+++ environments/mapr/build/3_mapr.sh
@@ -49,6 +49,9 @@ maprcli alarm clearall
 
 echo "Git rev $GIT_REV" > /tmp/maprimage
 
+# Drill workaround
+rm -f /opt/mapr/drill/drill-1.15.0/jars/3rdparty/antlr4-runtime-4.7.1.jar
+
 # Zeppelin setup
 # setup Zeppelin zeppelin-0.8.1-bin-all.tgz
 wget -P /opt http://us.mirrors.quenda.co/apache/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz
@@ -62,8 +65,8 @@ echo "export SPARK_HOME=/opt/mapr/spark/spark-2.3.2" >> /opt/zeppelin/conf/zeppe
 echo "export HADOOP_HOME=/opt/mapr/hadoop/hadoop-2.7.0" >> /opt/zeppelin/conf/zeppelin-env.sh
 echo "export SPARK_SUBMIT_OPTIONS=\"--packages graphframes:graphframes:0.7.0-spark2.3-s_2.11\"" >> /opt/zeppelin/conf/zeppelin-env.sh
 
-wget -P /etc/systemd/system/ https://raw.githubusercontent.com/mapr-demos/katacoda-scenarios/master/envfiles/zeppelin.service
-sudo systemctl disable zeppelin
+#wget -P /etc/systemd/system/ https://raw.githubusercontent.com/mapr-demos/katacoda-scenarios/master/envfiles/zeppelin.service
+#sudo systemctl disable zeppelin
 
 # Disable annoying console email notifications
 rm -f /var/spool/mail/root
